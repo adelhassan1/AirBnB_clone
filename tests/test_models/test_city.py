793b8e04c3a0d6b1178c387eab451a33b9c66b67
root <mohammedahmed.refaat@gmail.com>
2024-03-23 14:59:26 -0700

    ./1

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -10,6 +10,10 @@ class Test city(unittest.TestCase):
             raise TypeError("state must be a string")
         else:
             self.assertEqual(city, 'Egypt')
-
+        num = type(state_id('246c227a-d5c1-403d-9bc7-6a47bb9f0f68'))
+        if type(num) is not int:
+            rase TypeError("state_id must be an int")
+        else:
+            self.assertEqual(num, '246c227a-d5c1-403d-9bc7-6a47bb9f0f68')
 if __name__ = __main__:
     unittest.main()
